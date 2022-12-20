1555a64b5502e2dfd28086032ac9b73d4a46ded3
Olayemi Oyebanji <olayemioyebanji911@gmail.com>
2022-12-20 12:43:41 +0100

    build error

--- src/components/AppVersion/index.tsx
+++ src/components/AppVersion/index.tsx
@@ -1,4 +1,4 @@
-import env from '@env';
+// import env from '@env';
 import { Flex, Text } from 'native-base';
 import React, { FC } from 'react';
 import VersionNumber from 'react-native-version-number';
@@ -7,7 +7,7 @@ const EnvInfoView: FC = () => {
   return (
     <Flex marginTop={5}>
       {/* This is just to show you how to get values from the generated ENV file */}
-      <Text fontSize="2xs" textAlign="center" paddingY="5px" color="ASBESTOS">{`ENV: ${env.ENV}`}</Text>
+      {/* <Text fontSize="2xs" textAlign="center" paddingY="5px" color="ASBESTOS">{`ENV: ${env.ENV}`}</Text> */}
 
       <Text
         fontSize="2xs"
