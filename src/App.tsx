d4491810df0d1d8abced3583f236aaa8a1eb2ebb
BigUncleYemi <olayemioyebanji911@gmail.com>
2024-10-06 22:14:49 +0100

    Upgraded

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Splashscreen from '@components/Splashscreen';
 import env from '@env';
 import '@i18n';
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStackScreen } from '@routes';
 import { isMountedRef, navigationRef } from '@routes/navigationUtils';
-import { CaptureConsole } from '@sentry/integrations';
-import * as Sentry from '@sentry/react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import customTheme from '@theme';
 import { NativeBaseProvider, StatusBar } from 'native-base';
@@ -13,34 +12,35 @@ import React, { FC, Suspense, useEffect } from 'react';
 import { Platform } from 'react-native';
 import { Adjust, AdjustConfig } from 'react-native-adjust';
 import { AdjustOaid } from 'react-native-adjust-oaid';
-import 'react-native-gesture-handler';
 import { setCustomText, setCustomTextInput } from 'react-native-global-props';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { enableScreens } from 'react-native-screens';
 import SplashScreen from 'react-native-splash-screen';
 import RNUxcam from 'react-native-ux-cam';
-import { UXCamOcclusionType } from 'react-native-ux-cam/UXCamOcclusion';
 import { MixpanelProvider } from './MixpanelAnalytics';
 import NotificationContainer from './NotificationContainer';
 
-Sentry.init({
-  dsn: 'https://1e1707ce69834cf180a71be12fbcf40e@o4504776356003840.ingest.sentry.io/4504776371077120',
-  integrations: [
-    new CaptureConsole({
-      levels: ['error'],
-    }),
-  ],
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  // @ts-ignore
-  replaysSessionSampleRate: 0.3,
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-});
+// import { CaptureConsole } from '@sentry/integrations';
+// import * as Sentry from '@sentry/react-native';
+// Sentry.init({
+//   dsn: 'https://1e1707ce69834cf180a71be12fbcf40e@o4504776356003840.ingest.sentry.io/4504776371077120',
+//   integrations: [
+//     new CaptureConsole({
+//       levels: ['error'],
+//     }),
+//   ],
+//   // This sets the sample rate to be 10%. You may want this to be 100% while
+//   // in development and sample at a lower rate in production
+//   // @ts-ignore
+//   replaysSessionSampleRate: 0.3,
+//   // If the entire session is not sampled, use the below sample rate to sample
+//   // sessions when an error occurs.
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 enableScreens();
 
+// Setup for react-native-global-props
 const customTextProps = {
   allowFontScaling: false,
   style: {
@@ -78,7 +78,7 @@ const App: FC = () => {
     if (Platform.OS === 'android') {
       AdjustOaid.readOaid();
     }
-    Adjust.create(adjustConfig);
+    Adjust.initSdk(adjustConfig);
     return () => {
       Adjust.componentWillUnmount();
     };
@@ -87,7 +87,9 @@ const App: FC = () => {
   useEffect(() => {
     isMountedRef.current = true;
 
-    return () => (isMountedRef.current = false);
+    return () => {
+      isMountedRef.current = false;
+    };
   }, []);
 
   useEffect(() => {
@@ -104,10 +106,13 @@ const App: FC = () => {
 
   // Overlay over screens
   const overlay = {
-    type: UXCamOcclusionType.Overlay,
+    type: 2,
     color: 0x39ca95,
     hideGestures: true,
-    screens: ['LoginPage', 'SignUpPage', 'SetTransactionPin', 'LoginBackPage', 'WithdrawalPage', 'WithdrawalUSDT'],
+    screens: [
+      'LoginPage', 'SignUpPage', 'SetTransactionPin',
+      'LoginBackPage', 'WithdrawalPage', 'WithdrawalUSDT'
+    ],
   };
 
   RNUxcam.applyOcclusion(overlay);
